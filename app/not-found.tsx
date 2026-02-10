fd85492f75d435cf47f03a5dec66010dfcba5d69
ItzMrRatsP <newitzmrratsp@gmail.com>
2026-02-11 02:05:05 +0330

    kawaiii

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import style from "./modules/not-found.module.css";
 
 export default function Page() {
-	const path = usePathname();
 	return (
 		<div className="main">
 			<div className="top-background"></div>
-			{/* Remove padding between the title and description */}
 			<p className={style.text}>404</p>
 			<p className={style.description}>This page must be hiding in another dimension…</p>
 		</div>
